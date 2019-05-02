a2969804583cdaa997062c8caa5e3a89004093e2
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2019-05-02 11:03:25 -0500

    Create stackbuilder.sh

---
+++ stackbuilder.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+################################################################
+# Script_Name : stackbuilder.sh
+# Description : Perform an automated standard installation
+# of a container stack environment 
+# on ubuntu 18.04.1 and later
+# Date : may 2019
+# written by : tadeo
+# 
+# Version : 4.0
+# History : 0.3 - sourced by .bashrc
+
+# 0.1 - Initial Script
+# Disclaimer : Script provided AS IS. Use it at your own risk....
+##################################################################
+export CONTAINER_STACK_VER="4.0"
