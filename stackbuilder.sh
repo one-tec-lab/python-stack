b98ad9ad921bd3fa73880173c77db73563ed88b5
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2019-05-02 18:20:38 -0500

    Update stackbuilder.sh

--- stackbuilder.sh
+++ stackbuilder.sh
@@ -91,7 +91,8 @@ function stack-up {
     echo
 
 
-    #docker-compose run app django-admin startproject project
+    docker-compose run app django-admin startproject project
+    docker-compose down --remove-orphans
     
     MYSQL_ROOT_PASSWORD=$mysqlrootpassword \
     MYSQL_PASSWORD=$dbuserpassword \
