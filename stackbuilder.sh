2583d29dffe4e8f0d71b87e04eda4e4c92f829e7
Tadeo Gutiérrez <tadeogutierrezsanchez@gmail.com>
2019-05-02 11:04:37 -0500

    Update stackbuilder.sh

--- stackbuilder.sh
+++ stackbuilder.sh
@@ -15,3 +15,22 @@
 # Disclaimer : Script provided AS IS. Use it at your own risk....
 ##################################################################
 export CONTAINER_STACK_VER="4.0"
+os=${OSTYPE//[0-9.-]*/}
+
+case "$os" in
+  darwin)
+    echo "I'm a Mac"
+    ;;
+
+  msys)
+    echo "I'm Windows using git bash"
+    ;;
+
+  linux)
+    echo "I'm Linux"
+    ;;
+  *)
+
+  echo "Unknown Operating system $OSTYPE"
+  exit 1
+esac
